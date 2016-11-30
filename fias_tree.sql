d1e8fd734bcdf1256f8e01a131556c7a1c0fa74b
Mikhail Che <ernesto.chege@yandex.ru>
2016-11-30 14:47:15 +0500

    коммит

--- fias_tree.sql
+++ fias_tree.sql
@@ -14,14 +14,21 @@ CREATE OR REPLACE FUNCTION fias.f_AddressObjectTree(
                                                    /* из прежних его наименований, */
                                                    /* 51 - переподчиненный */
 )
-RETURNS TABLE (rtf_AOGUID uuid, rtf_CurrStatus INTEGER, rtf_ActStatus INTEGER, 
-                                rtf_AOLevel INTEGER,rtf_ShortTypeName VARCHAR(10),
-                                rtf_AddressObjectName VARCHAR(100)) AS
+RETURNS TABLE (
+ "AOID" uuid,
+ "AOGUID" uuid,
+ "CURRSTATUS" INTEGER,
+ "ACTSTATUS" INTEGER, 
+ "AOLEVEL" INTEGER,
+ "SHORTNAME" VARCHAR(10),
+ "FORMALNAME" text
+) AS
 $BODY$
 DECLARE
  c_ActualStatusCode CONSTANT INTEGER :=1; /* Признак актуальной записи  */
                                     /* адресообразующего элемента */
  c_NotActualStatusCode CONSTANT INTEGER :=0;	/* Значение кода актуальной записи */
+ v_AOID     uuid;	 /* ИД адресообразующего элемента */
  v_AOGUID     uuid;	 /* ИД адресообразующего элемента */
  v_ParentGUID uuid; /* Идентификатор родительского элемента */
  v_CurrStatus    INTEGER; /* Статус актуальности КЛАДР 4*/
@@ -29,58 +36,58 @@ DECLARE
                                     /* адресообразующего элемента ФИАС. */
  v_AOLevel      INTEGER; /*Уровень адресообразующего элемента  */
  v_ShortName  VARCHAR(10); /* Краткое наименование типа элемента */
- v_FormalName VARCHAR(120); /* Формализованное наименование элемента */
+ v_FormalName text; /* Формализованное наименование элемента */
  v_Return_Error INTEGER;  /* Код возврата */
 /***********************************************************************/
  BEGIN
  IF a_CurrStatus IS NOT NULL THEN
     SELECT INTO 
-    v_AOGUID,v_ParentGUID,v_CurrStatus,v_ActStatus,v_AOLevel, v_ShortName, v_FormalName
-    ao."AOGUID",ao."PARENTGUID",ao."CURRSTATUS",ao."ACTSTATUS",ao."AOLEVEL", ao."SHORTNAME", ao."FORMALNAME"
-    FROM fias.AddressObjects ao
+    v_AOID, v_AOGUID, v_ParentGUID, v_CurrStatus, v_ActStatus, v_AOLevel, v_ShortName, v_FormalName
+    ao."AOID", ao."AOGUID",ao."PARENTGUID",ao."CURRSTATUS",ao."ACTSTATUS",ao."AOLEVEL", ao."SHORTNAME", ao."FORMALNAME"
+    FROM fias."AddressObjects" ao
     WHERE ao."AOGUID"=a_AOGUID AND ao."CURRSTATUS"=a_CurrStatus;
  ELSE
     SELECT INTO
-    v_AOGUID,v_ParentGUID,v_CurrStatus,v_ActStatus,v_AOLevel, v_ShortName, v_FormalName
-    ao."AOGUID",ao."PARENTGUID",ao."CURRSTATUS",ao."ACTSTATUS",ao."AOLEVEL", ao."SHORTNAME", ao."FORMALNAME"
-    FROM fias.AddressObjects ao
+    v_AOID, v_AOGUID,v_ParentGUID,v_CurrStatus,v_ActStatus,v_AOLevel, v_ShortName, v_FormalName
+    ao."AOID", ao."AOGUID",ao."PARENTGUID",ao."CURRSTATUS",ao."ACTSTATUS",ao."AOLEVEL", ao."SHORTNAME", ao."FORMALNAME"
+    FROM fias."AddressObjects" ao
     WHERE ao."AOGUID"=a_AOGUID AND ao."ACTSTATUS"=c_ActualStatusCode;
   ---------------------------------------------------------------
    IF NOT FOUND THEN
       SELECT INTO
-      v_AOGUID,v_ParentGUID,v_CurrStatus,v_ActStatus,v_AOLevel, v_ShortName, v_FormalName
-      ao."AOGUID",ao."PARENTGUID",ao."CURRSTATUS",ao."ACTSTATUS",ao."AOLEVEL", ao."SHORTNAME", ao."FORMALNAME"
-      FROM fias.AddressObjects ao
+      v_AOID, v_AOGUID,v_ParentGUID,v_CurrStatus,v_ActStatus,v_AOLevel, v_ShortName, v_FormalName
+      ao."AOID", ao."AOGUID",ao."PARENTGUID",ao."CURRSTATUS",ao."ACTSTATUS",ao."AOLEVEL", ao."SHORTNAME", ao."FORMALNAME"
+      FROM fias."AddressObjects" ao
       WHERE ao."AOGUID"=a_AOGUID 
          AND ao."ACTSTATUS"=c_NotActualStatusCode
          AND ao."CURRSTATUS" = (SELECT MAX(iao."CURRSTATUS") 
-                                          FROM fias.AddressObjects iao 
+                                          FROM fias."AddressObjects" iao 
                                           WHERE ao."AOGUID" = iao."AOGUID");
     END IF;
  END IF;
  RETURN QUERY
- SELECT v_AOGUID,v_CurrStatus,v_ActStatus,v_AOLevel, v_ShortName,v_FormalName;
+ SELECT v_AOID, v_AOGUID,v_CurrStatus,v_ActStatus,v_AOLevel, v_ShortName,v_FormalName;
  ----------------------------------------------------------------
  WHILE  v_ParentGUID IS NOT NULL LOOP
      SELECT INTO
-     v_AOGUID,v_ParentGUID,v_CurrStatus,v_ActStatus,v_AOLevel, v_ShortName, v_FormalName
-     ao."AOGUID",ao."PARENTGUID",ao."CURRSTATUS",ao."ACTSTATUS",ao."AOLEVEL", ao."SHORTNAME",ao."FORMALNAME"
-     FROM fias.AddressObjects ao
+     v_AOID, v_AOGUID,v_ParentGUID,v_CurrStatus,v_ActStatus,v_AOLevel, v_ShortName, v_FormalName
+     ao."AOID", ao."AOGUID",ao."PARENTGUID",ao."CURRSTATUS",ao."ACTSTATUS",ao."AOLEVEL", ao."SHORTNAME",ao."FORMALNAME"
+     FROM fias."AddressObjects" ao
      WHERE ao."AOGUID"=v_ParentGUID AND ao."ACTSTATUS"=c_ActualStatusCode;
      ---------------------------------------------------
       IF NOT FOUND THEN   
          SELECT INTO
-         v_AOGUID,v_ParentGUID,v_CurrStatus,v_ActStatus,v_AOLevel, v_ShortName,v_FormalName
-         ao."AOGUID",ao."PARENTGUID",ao."CURRSTATUS",ao."ACTSTATUS",ao."AOLEVEL", ao."SHORTNAME",ao."FORMALNAME"
-             FROM fias.AddressObjects ao
+         v_AOID, v_AOGUID,v_ParentGUID,v_CurrStatus,v_ActStatus,v_AOLevel, v_ShortName,v_FormalName
+         ao."AOID", ao."AOGUID",ao."PARENTGUID",ao."CURRSTATUS",ao."ACTSTATUS",ao."AOLEVEL", ao."SHORTNAME",ao."FORMALNAME"
+             FROM fias."AddressObjects" ao
              WHERE ao."AOGUID"=v_ParentGUID 
                   AND ao."ACTSTATUS"=c_NotActualStatusCode
                   AND ao."CURRSTATUS" = (SELECT MAX(iao."CURRSTATUS") 
-                                                   FROM fias.AddressObjects iao 
+                                                   FROM fias."AddressObjects" iao 
                                                    WHERE ao."AOGUID" = iao."AOGUID");
       END IF;	
       RETURN QUERY
-      SELECT v_AOGUID,v_CurrStatus,v_ActStatus,v_AOLevel,v_ShortName, v_FormalName;
+      SELECT v_AOID, v_AOGUID,v_CurrStatus,v_ActStatus,v_AOLevel,v_ShortName, v_FormalName;
  END LOOP;
 END;
 $BODY$
@@ -91,6 +98,6 @@ COMMENT ON FUNCTION fias.f_AddressObjectTree(a_AOGUID uuid,
 IS 'Возвращает дерево (список взаимосвязанных строк) 
      с характеристиками адресообразующего элемента
      
-     SELECT * FROM fias.f_AddressObjectTree(''719b789d-2476-430a-89cd-3fedc643d821'',51) ORDER BY rtf_AOLevel;
-     SELECT * FROM fias.f_AddressObjectTree(''719b789d-2476-430a-89cd-3fedc643d821'') ORDER BY rtf_AOLevel;
+     SELECT * FROM fias.f_AddressObjectTree(''719b789d-2476-430a-89cd-3fedc643d821'',51) ORDER BY "AOLEVEL";
+     SELECT * FROM fias.f_AddressObjectTree(''719b789d-2476-430a-89cd-3fedc643d821'') ORDER BY "AOLEVEL";
      ';
--- indexes.sql
+++ indexes.sql
@@ -1,17 +1,25 @@
-ALTER TABLE fias."AddressObjects" ADD CONSTRAINT AddressObjects_pkey PRIMARY KEY(aoguid);
+ALTER TABLE fias."AddressObjects" ADD CONSTRAINT AddressObjects_pkey PRIMARY KEY("id");
+CREATE UNIQUE INDEX ON fias."AddressObjects" USING btree ("AOID");
+CREATE INDEX ON fias."AddressObjects" USING btree ("AOGUID");
+--CREATE UNIQUE INDEX ON fias."AddressObjects" USING btree ("AOGUID", "ACTSTATUS", "CURRSTATUS", "LIVESTATUS");
+CREATE INDEX ON fias."AddressObjects" USING btree ("PARENTGUID");
+
 
 -- foreign key (parentguid to aoguid)
 -- ALTER TABLE addrobj DROP CONSTRAINT addrobj_parentguid_fkey;
+--ОШИБКА:  в целевой внешней таблице "AddressObjects" нет ограничения уникальности, соответствующего данным ключам
+
 ALTER TABLE fias."AddressObjects" 
   ADD CONSTRAINT AddressObjects_parentguid_fkey FOREIGN KEY ("PARENTGUID")
-  REFERENCES fias."AddressObjects" ("AOGUID") MATCH SIMPLE
-ON UPDATE CASCADE ON DELETE NO ACTION;
+  REFERENCES fias."AddressObjects" ("AOGUID")
+  MATCH SIMPLE
+  ON UPDATE CASCADE ON DELETE NO ACTION;
 
 
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
+CREATE INDEX on fias."AddressObjects" USING gin (lower("FORMALNAME") gin_trgm_ops);
 
--- CREATE UNIQUE INDEX aoid_idx ON addrobj USING btree (aoid);
-CREATE INDEX parentguid_idx ON fias."AddressObjects" USING btree ("PARENTGUID");
+
 CREATE INDEX currstatus_idx ON addrobj USING btree (currstatus);
 CREATE INDEX aolevel_idx ON addrobj USING btree (aolevel);
 CREATE INDEX formalname_idx ON addrobj USING btree (formalname);
@@ -20,6 +28,21 @@ CREATE INDEX shortname_idx ON addrobj USING btree (shortname);
 CREATE INDEX shortname_aolevel_idx ON addrobj USING btree (shortname, aolevel);
 
 
--- trigram indexes to speed up text searches
-CREATE INDEX formalname_trgm_idx on addrobj USING gin (formalname gin_trgm_ops);
-CREATE INDEX offname_trgm_idx on addrobj USING gin (offname gin_trgm_ops);
+
+CREATE OR REPLACE FUNCTION fias.aoid_parents(uuid)
+RETURNS  SETOF fias."AddressObjects"
+AS $$
+-- вывести полный адрес
+WITH RECURSIVE child_to_parents AS (
+  SELECT a.*
+  FROM fias."AddressObjects" a
+  WHERE a."AOID" = $1--'51f21baa-c804-4737-9d5f-9da7a3bb1598'
+UNION ALL
+  SELECT a.*
+  FROM fias."AddressObjects" a, child_to_parents c
+  WHERE a."AOGUID" = c."PARENTGUID"
+        --AND a."CURRSTATUS" = 0
+        AND a."ACTSTATUS" = 1
+)
+SELECT * FROM child_to_parents ORDER BY "AOLEVEL";
+$$ LANGUAGE SQL;
