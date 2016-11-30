40a4b37702dc554515b499df27bf8bc31aefe27e
Mikhail Che <ernesto.chege@yandex.ru>
2016-11-30 12:52:49 +0500

    коммит

--- fias_tree.sql
+++ fias_tree.sql
@@ -34,56 +34,53 @@ DECLARE
 /***********************************************************************/
  BEGIN
  IF a_CurrStatus IS NOT NULL THEN
-    SELECT INTO  v_AOGUID,v_ParentGUID,v_CurrStatus,v_ActStatus,v_AOLevel,
-                              v_ShortName, v_FormalName
-                               ao.AOGUID,ao.ParentGUID,ao.CurrStatus,ao.ActStatus,ao.AOLevel,
-                              ao.ShortName, ao.FormalName
-                  FROM fias.AddressObjects ao
-  WHERE ao.AOGUID=a_AOGUID AND ao.CurrStatus=a_CurrStatus;
+    SELECT INTO 
+    v_AOGUID,v_ParentGUID,v_CurrStatus,v_ActStatus,v_AOLevel, v_ShortName, v_FormalName
+    ao."AOGUID",ao."PARENTGUID",ao."CURRSTATUS",ao."ACTSTATUS",ao."AOLEVEL", ao."SHORTNAME", ao."FORMALNAME"
+    FROM fias.AddressObjects ao
+    WHERE ao."AOGUID"=a_AOGUID AND ao."CURRSTATUS"=a_CurrStatus;
  ELSE
-    SELECT INTO v_AOGUID,v_ParentGUID,v_CurrStatus,v_ActStatus,v_AOLevel,
-                              v_ShortName, v_FormalName
-                              ao.AOGUID,ao.ParentGUID,ao.CurrStatus,ao.ActStatus,ao.AOLevel,
-                              ao.ShortName, ao.FormalName
-                   FROM fias.AddressObjects ao
-	WHERE ao.AOGUID=a_AOGUID AND ao.ActStatus=c_ActualStatusCode;
+    SELECT INTO
+    v_AOGUID,v_ParentGUID,v_CurrStatus,v_ActStatus,v_AOLevel, v_ShortName, v_FormalName
+    ao."AOGUID",ao."PARENTGUID",ao."CURRSTATUS",ao."ACTSTATUS",ao."AOLEVEL", ao."SHORTNAME", ao."FORMALNAME"
+    FROM fias.AddressObjects ao
+    WHERE ao."AOGUID"=a_AOGUID AND ao."ACTSTATUS"=c_ActualStatusCode;
+  ---------------------------------------------------------------
    IF NOT FOUND THEN
-      SELECT INTO v_AOGUID,v_ParentGUID,v_CurrStatus,v_ActStatus,v_AOLevel,
-                               v_ShortName, v_FormalName
-                                  ao.AOGUID,ao.ParentGUID,ao.CurrStatus,ao.ActStatus,ao.AOLevel,
-                                ao.ShortName, ao.FormalName
-              FROM fias.AddressObjects ao
-              WHERE ao.AOGUID=a_AOGUID 
-                       AND ao.ActStatus=c_NotActualStatusCode
-                      AND ao.currstatus = (SELECT MAX(iao.currstatus) 
-                                                       FROM fias.AddressObjects iao 
-                                                       WHERE ao.aoguid = iao.aoguid);
+      SELECT INTO
+      v_AOGUID,v_ParentGUID,v_CurrStatus,v_ActStatus,v_AOLevel, v_ShortName, v_FormalName
+      ao."AOGUID",ao."PARENTGUID",ao."CURRSTATUS",ao."ACTSTATUS",ao."AOLEVEL", ao."SHORTNAME", ao."FORMALNAME"
+      FROM fias.AddressObjects ao
+      WHERE ao."AOGUID"=a_AOGUID 
+         AND ao."ACTSTATUS"=c_NotActualStatusCode
+         AND ao."CURRSTATUS" = (SELECT MAX(iao."CURRSTATUS") 
+                                          FROM fias.AddressObjects iao 
+                                          WHERE ao."AOGUID" = iao."AOGUID");
     END IF;
  END IF;
  RETURN QUERY
  SELECT v_AOGUID,v_CurrStatus,v_ActStatus,v_AOLevel, v_ShortName,v_FormalName;
- 
+ ----------------------------------------------------------------
  WHILE  v_ParentGUID IS NOT NULL LOOP
-     SELECT INTO v_AOGUID,v_ParentGUID,v_CurrStatus,v_ActStatus,v_AOLevel,
-                              v_ShortName, v_FormalName
-                           ao.AOGUID,ao.ParentGUID,ao.CurrStatus,ao.ActStatus,ao.AOLevel,
-                             ao.ShortName,ao.FormalName
-         FROM fias.AddressObjects ao
-         WHERE ao.AOGUID=v_ParentGUID AND ao.ActStatus=c_ActualStatusCode;
-          IF NOT FOUND THEN   
-             SELECT INTO v_AOGUID,v_ParentGUID,v_CurrStatus,v_ActStatus,v_AOLevel,
-                                        v_ShortName,v_FormalName
-                             ao.AOGUID,ao.ParentGUID,ao.CurrStatus,ao.ActStatus,ao.AOLevel,
-                                        ao.ShortName, ao.FormalName
-                 FROM fias.AddressObjects ao
-                 WHERE ao.AOGUID=v_ParentGUID 
-                               AND ao.ActStatus=c_NotActualStatusCode
-                              AND ao.currstatus = (SELECT MAX(iao.currstatus) 
-                                                               FROM fias.AddressObjects iao 
-                                                               WHERE ao.aoguid = iao.aoguid);
-          END IF;	
-          RETURN QUERY
-          SELECT v_AOGUID,v_CurrStatus,v_ActStatus,v_AOLevel,v_ShortName, v_FormalName;
+     SELECT INTO
+     v_AOGUID,v_ParentGUID,v_CurrStatus,v_ActStatus,v_AOLevel, v_ShortName, v_FormalName
+     ao."AOGUID",ao."PARENTGUID",ao."CURRSTATUS",ao."ACTSTATUS",ao."AOLEVEL", ao."SHORTNAME",ao."FORMALNAME"
+     FROM fias.AddressObjects ao
+     WHERE ao."AOGUID"=v_ParentGUID AND ao."ACTSTATUS"=c_ActualStatusCode;
+     ---------------------------------------------------
+      IF NOT FOUND THEN   
+         SELECT INTO
+         v_AOGUID,v_ParentGUID,v_CurrStatus,v_ActStatus,v_AOLevel, v_ShortName,v_FormalName
+         ao."AOGUID",ao."PARENTGUID",ao."CURRSTATUS",ao."ACTSTATUS",ao."AOLEVEL", ao."SHORTNAME",ao."FORMALNAME"
+             FROM fias.AddressObjects ao
+             WHERE ao."AOGUID"=v_ParentGUID 
+                  AND ao."ACTSTATUS"=c_NotActualStatusCode
+                  AND ao."CURRSTATUS" = (SELECT MAX(iao."CURRSTATUS") 
+                                                   FROM fias.AddressObjects iao 
+                                                   WHERE ao."AOGUID" = iao."AOGUID");
+      END IF;	
+      RETURN QUERY
+      SELECT v_AOGUID,v_CurrStatus,v_ActStatus,v_AOLevel,v_ShortName, v_FormalName;
  END LOOP;
 END;
 $BODY$
@@ -92,10 +89,8 @@ $BODY$
 COMMENT ON FUNCTION fias.f_AddressObjectTree(a_AOGUID uuid, 
              a_CurrStatus INTEGER)
 IS 'Возвращает дерево (список взаимосвязанных строк) 
-     с характеристиками адресообразующего элемента';
---ROLLBACK TRANSACTION;
---COMMIT TRANSACTION;
----SELECT * FROM fias.f_AddressObjectTree('719b789d-2476-430a-89cd-3fedc643d821',51) 
---            ORDER BY rtf_AOLevel;
---SELECT * FROM fias.f_AddressObjectTree('719b789d-2476-430a-89cd-3fedc643d821')
---            ORDER BY rtf_AOLevel;
+     с характеристиками адресообразующего элемента
+     
+     SELECT * FROM fias.f_AddressObjectTree(''719b789d-2476-430a-89cd-3fedc643d821'',51) ORDER BY rtf_AOLevel;
+     SELECT * FROM fias.f_AddressObjectTree(''719b789d-2476-430a-89cd-3fedc643d821'') ORDER BY rtf_AOLevel;
+     ';
